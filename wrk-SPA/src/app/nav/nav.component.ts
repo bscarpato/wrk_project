d005bcd17c2020017a0424c55bfa91e472ac2ffa
Bruno Scarpato <bsscarpato@hotmail.com>
2019-08-14 07:04:19 -0300

    added alertify and jwtHelperService

--- wrk-SPA/src/app/nav/nav.component.ts
+++ wrk-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,27 +10,26 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Login com sucesso!');
+      this.alertify.success('Login com sucesso!');
     }, error => {
-      console.log('Falha no login!');
+      this.alertify.error('Falha no login!');
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('Logout!');
+    this.alertify.message('Logout!');
   }
 
 }
