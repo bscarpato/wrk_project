79ac1d2465b47fb4df4c899e7c171d581ae7169f
Bruno Scarpato <bsscarpato@hotmail.com>
2019-08-16 07:48:37 -0300

    added bootswatch theme and new JwtHelper to manage the token

--- wrk-SPA/src/app/_services/auth.service.ts
+++ wrk-SPA/src/app/_services/auth.service.ts
@@ -9,6 +9,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) { }
 
@@ -19,6 +20,8 @@ export class AuthService {
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.decodedToken);
           }
         })
       );
@@ -28,7 +31,7 @@ export class AuthService {
     return this.http.post(this.baseUrl + 'register', model);
   }
 
-  loggedIn(){
+  loggedIn() {
     const token = localStorage.getItem('token');
     return !this.jwtHelper.isTokenExpired(token);
   }
