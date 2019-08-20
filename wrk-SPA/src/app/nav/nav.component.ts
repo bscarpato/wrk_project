029d9f47737363db96d65fbe728b8b32d40e0c0c
Bruno Scarpato <bsscarpato@hotmail.com>
2019-08-20 08:29:36 -0300

    added routes and auth guard

--- wrk-SPA/src/app/nav/nav.component.ts
+++ wrk-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,16 +11,19 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  // direciona pelo route para a pagina de membros
   login() {
     this.authService.login(this.model).subscribe(next => {
       this.alertify.success('Login com sucesso!');
     }, error => {
       this.alertify.error('Falha no login!');
+    }, () => {
+      this.router.navigate(['/home']);
     });
   }
 
@@ -30,6 +34,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logout!');
+    this.router.navigate(['/home']);
   }
 
 }
