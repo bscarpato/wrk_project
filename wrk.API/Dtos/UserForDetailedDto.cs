bd6ded75f19c0a2b617a273114b0c0ebf4e401c7
Bruno Scarpato <bsscarpato@hotmail.com>
2019-08-27 06:54:23 -0300

    mapped Photos and Age to fix the value.

--- wrk.API/Dtos/UserForDetailedDto.cs
+++ wrk.API/Dtos/UserForDetailedDto.cs
@@ -18,6 +18,6 @@ namespace wrk.API.Dtos
         public string City { get; set; }
         public string Country { get; set; }
         public string PhotoUrl { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public ICollection<PhotoForDetailedDto> Photos { get; set; }
     }
 }
